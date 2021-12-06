a44b5aedda00e4733dcb5b5a9bc94ba849fcd07b
Mohammed Ahmed <2149869@talnet.nl>
2021-12-06 09:47:40 +0100

    aaa

--- 2-Variabelen-en-datatype/2.3 - Datatype integer & float/index.php
+++ 2-Variabelen-en-datatype/2.3 - Datatype integer & float/index.php
@@ -3,12 +3,13 @@
 <body>
 
 <?php
-$leeftijd = 17;
+//$leeftijd = 17;
+//echo $leeftijd;
+//echo "<br>";
+//$melkprijs = 0.98;
+//echo $melkprijs;
 $naam = "Mohammed";
-echo $leeftijd;
-echo "<br>";
-$melkprijs = 0.98;
-echo $melkprijs;
+echo $naam;
 ?>
 
 </body>
