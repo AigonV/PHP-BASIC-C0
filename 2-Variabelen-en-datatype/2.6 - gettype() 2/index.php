af7f0dd7efaacdf748df297a949d70a890751854
Mohammed Ahmed <2149869@talnet.nl>
2021-12-06 09:58:45 +0100

    yuhhhh

---
+++ 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$testString = "testString";
+$testFloat = 0.66;
+$testBoolean = true;
+$testNull = null;
+
+echo gettype($testString);
+echo "<br>";
+echo gettype($testFloat);
+echo "<br>";
+echo gettype($testBoolean);
+echo "<br>";
+echo gettype($testNull);
+?>
+
+</body>
+</html> 
