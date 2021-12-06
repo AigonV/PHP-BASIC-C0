d4b33df18144527ea4a3986ee16f520e8a7ac461
Mohammed Ahmed <2149869@talnet.nl>
2021-12-06 10:52:56 +0100

    aaaaaaa

---
+++ 2-Variabelen-en-datatype/2.8 - data en html combineren/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$voornaam1 = "Aydin";
+$voornaam2 = "Chahid";
+$voornaam3 = "Jaren";
+
+echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>"
+?>
+</body>
+</html> 
