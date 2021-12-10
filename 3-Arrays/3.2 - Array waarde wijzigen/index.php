744f8353be68954080976adb689a65002db51463
Mohammed Ahmed <2149869@talnet.nl>
2021-12-10 12:50:41 +0100

    added code

---
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$myDogNames = array(
+    "Aydin",
+    "Chahid",
+    "Jaren"
+);
+
+$myDogNames[0] = "Peepee";
+$myDogNames[2] = "Michael";
+
+echo "<li>$myDogNames[0]</li><li>$myDogNames[1]</li><li>$myDogNames[2]</li>"
+?>
+
+</body>
+</html>
