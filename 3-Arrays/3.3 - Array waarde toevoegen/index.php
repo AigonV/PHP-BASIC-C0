d5e9bd2ef6af7f4a11aabafca9234f97c4cf81a1
Mohammed Ahmed <2149869@talnet.nl>
2021-12-10 12:59:09 +0100

    added code

---
+++ 3-Arrays/3.3 - Array waarde toevoegen/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$myHeroes = array(
+    "Captain America",
+    "Iron Man",
+    "Shinra"
+);
+
+$myHeroes[3] = "Midoriya";
+array_push($myHeroes, "Lemillion");
+
+print_r($myHeroes);
+?>
+
+</body>
+</html>
