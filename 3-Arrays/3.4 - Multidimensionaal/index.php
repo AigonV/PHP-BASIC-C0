d5598946e35b4807eac2293784f46d3cb8b36b7f
Mohammed Ahmed <2149869@talnet.nl>
2021-12-13 09:56:07 +0100

    added code

---
+++ 3-Arrays/3.4 - Multidimensionaal/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$abcGetallen = array(
+    "a",
+    "b",
+    "c",
+    "d"
+);
+
+$getallen = array(
+    "24",
+    "56",
+    "78",
+    "90"
+);
+
+print_r("a".$getallen[1]."<br>");
+print_r("c".$getallen[3]."<br>");
+?>
+
+</body>
+</html>
