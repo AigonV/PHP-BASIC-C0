f80afb0fe59bdffec7dfc4afd577a2fa7dfc7f83
Mohammed Ahmed <2149869@talnet.nl>
2021-12-14 09:14:43 +0100

    added code

---
+++ 4-Arithmetics/4.1 - Arithmetic Operators/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$getalEen = 6;
+$getalTwee = 9;
+echo $getalEen + $getalTwee;
+echo "<br>";
+
+$getalEen = 4;
+$getalTwee = 20;
+echo $getalEen - $getalTwee;
+echo "<br>";
+
+$getalEen = 8;
+$getalTwee = 2;
+echo $getalEen * $getalTwee;
+echo "<br>";
+
+$getalEen = 2;
+$getalTwee = 10;
+echo $getalEen / $getalTwee;
+
+?>
+</body>
+</html>
