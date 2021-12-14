233d675f6d42420639f9b802fc824f01f749fc61
Mohammed Ahmed <2149869@talnet.nl>
2021-12-14 09:42:20 +0100

    sss

---
+++ 4-Arithmetics/4.2 -Assignment Operators/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$getal1 = 6;
+$getal1 += 77;
+echo $getal1;
+echo "<br>";
+
+
+$getal2 = 2;
+$getal2 -= 1330;
+echo $getal2;
+echo "<br>";
+
+$getal3 = 7;
+$getal3 *= 44;
+echo $getal3;
+echo "<br>";
+
+$getal4 = 44;
+$getal4 /= 555;
+echo $getal4;
+
+?>
+</body>
+</html>
