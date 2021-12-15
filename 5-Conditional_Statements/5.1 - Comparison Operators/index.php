cc4ad1a84f291495c88eba8ab154af07705f30da
Mohammed Ahmed <2149869@talnet.nl>
2021-12-15 10:52:28 +0100

    added code

---
+++ 5-Conditional_Statements/5.1 - Comparison Operators/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$test1= 9;
+$test2= "text noob";
+var_dump($test1 == $test2);
+echo "<br>";
+var_dump($test1 === $test2);
+echo "<br>";
+var_dump($test1 != $test2);
+echo "<br>";
+var_dump($test1 < $test2);
+echo "<br>";
+var_dump($test1 > $test2);
+echo "<br>";
+var_dump($test1 >= $test2);
+echo "<br>";
+var_dump($test1 <> $test2);
+echo "<br>";
+var_dump($test1 >= $test2);
+?>
+</body>
+</html>
