24fbd924eb645a27bcd93583a023e48d0fb01215
Mohammed Ahmed <2149869@talnet.nl>
2021-12-17 12:56:09 +0100

    aaaaaa

---
+++ 6-Functions/6.2 - strlen()/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+function mijnNaamIsGroot(){
+    $voornaam = "Aydin";
+    $achternaam = "Maleki";
+    $geheleNaam= "Mijn naam is $voornaam $achternaam";
+    echo strtoupper($geheleNaam);
+}
+mijnNaamIsGroot();
+
+?>
+</body>
+</html>
