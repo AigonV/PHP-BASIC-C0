225be7042e775c7aec87d52e4f89380afa0fb0c5
Mohammed Ahmed <2149869@talnet.nl>
2021-12-17 12:54:40 +0100

    qwfffe

---
+++ 6-Functions/6.1 - function declaration/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+function zetTweeWoordenAanElkaar(){
+    $woordje = "Hallo";
+    $zinnetje = "mooie blauwe planeet!";
+    $helezin = $woordje . " " . $zinnetje;
+    echo $helezin;
+}
+zetTweeWoordenAanElkaar();
+
+?>
+</body>
+</html>
